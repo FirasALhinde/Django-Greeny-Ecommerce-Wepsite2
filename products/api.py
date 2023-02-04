b51049bf8b4834d03a8f98b037a8bf77e0479f57
unknown <you@example.com>
2023-02-04 14:51:35 +0400

    work api file

--- products/api.py
+++ products/api.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from .serializers  import ProductSerializer
 from . models import Product
 
-
+# function
 @api_view(['Get'])
 def product_list_api(request):
     products = Product.objects.all()
@@ -13,7 +13,7 @@ def product_list_api(request):
     return Response({'Success':True,'ProuductList':data})
 
 
-
+# genraic class
 class ProuductListApi(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all().filter(pk=2)
